a404d8cb5f41b28f32ee2cf20bb71cd3f5373d78
Renata Dimas <renatagdimas@gmail.com>
2023-08-11 16:30:37 -0300

    Create ex2B.py
    Exercício número 2 letra B

---
+++ MODULO 1/ex2B.py
@@ -0,0 +1,10 @@
+import random
+num = random.randint(1,100)
+escolha = int(input('Escolha o número: '))
+while escolha != num:
+    if num > escolha:
+        print('O número escolhido é menor do que o número secreto!')
+    else:
+        print('O número escolhido é maior do que o número secreto!')
+    escolha = int(input('Escolha o número: '))
+print(f'Você acertou!! O número secreto é {num}')
